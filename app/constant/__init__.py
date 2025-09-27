319878ce5952a42a4651d68cb60005d54bd83da9
Tom <tom@fake.mail>
2025-09-27 23:55:05 +0200

    feat(content_file_path): ability to fetch file content paths

--- app/constant/__init__.py
+++ app/constant/__init__.py
@@ -1,2 +1,2 @@
 from .swagger_tags import master, dev, system
-from .url import health_strip, api_strip, v1_strip, example_strip
+from .url import health_strip, api_strip, v1_strip, send_content_file_parent_dir_strip
--- app/constant/url.py
+++ app/constant/url.py
@@ -1,4 +1,4 @@
 health_strip: str = "/health"
 api_strip: str = "/api"
 v1_strip: str = "/v1"
-example_strip: str = "/example"
+send_content_file_parent_dir_strip: str = "/send_content_file_parent_dir"
