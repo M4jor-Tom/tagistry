6fc53da0687318c1ead245c6f474be330b352056
Tom <tom@fake.mail>
2025-09-28 21:03:12 +0200

    feat(rule_set service): use categories_inheritance to check on rule set consistency

--- app/exception/__init__.py
+++ app/exception/__init__.py
@@ -1,3 +1,4 @@
 from .domain_exception import DomainException
 from .content_import_exception import ContentImportException, FileNameWithoutSpaces, UntaggedContentFile
-from .rule_set_import_exception import RuleSetImportException, RuleSetElementPathDoesNotExist
+from .rule_set_import_exception import RuleSetImportException, RuleSetElementPathDoesNotExist, \
+    InconsistentRuleSetException
