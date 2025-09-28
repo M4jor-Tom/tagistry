51e90814e15a7c5fb54afaa639569fd836bb4d3a
Tom <tom@fake.mail>
2025-09-28 11:07:06 +0200

    refactor(name): name project 'tagistry'

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from core.config import setup_logging, LOGGING_LEVEL, APP_HOST, APP_PORT
 if __name__ == "__main__":
     import uvicorn
     setup_logging(LOGGING_LEVEL)
-    uvicorn.run("core.fastapi_skeleton:app", host=APP_HOST, port=APP_PORT, reload=False, workers=1)
+    uvicorn.run("core.tagistry:app", host=APP_HOST, port=APP_PORT, reload=False, workers=1)
