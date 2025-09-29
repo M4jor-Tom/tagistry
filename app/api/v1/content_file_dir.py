39901609d97e269863b57a4567dad67f58ca9178
Tom <tom@fake.mail>
2025-09-29 23:22:53 +0200

    feat(env): provide default dirs for content_file and rule_set

--- app/api/v1/content_file_dir.py
+++ app/api/v1/content_file_dir.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
 
-from core.config import CONTENT_FILE_PATH_BLACKLIST_STRIP
+from core.config import DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP, DEFAULT_CONTENT_FILE_DIR
 from constant import master, send_content_file_parent_dir_strip
 from service import ContentFileService, get_content_file_service
 
@@ -10,9 +10,9 @@ content_file_dir_router = APIRouter()
 
 @content_file_dir_router.post(send_content_file_parent_dir_strip, tags=[master])
 def send_content_file_parent_dir(
-        content_file_absolute_parent_dir: str,
-        banned_strips: tuple[str, ...] = CONTENT_FILE_PATH_BLACKLIST_STRIP,
-        compute_hash: bool = True,
+        content_file_absolute_parent_dir: str = DEFAULT_CONTENT_FILE_DIR,
+        banned_strips: tuple[str, ...] = DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP,
+        compute_hash: bool = False,
         content_file_service: ContentFileService = Depends(get_content_file_service)) -> None:
     content_file_service.input_content_files(
         content_file_absolute_parent_dir,
--- app/api/v1/rule_set_dir.py
+++ app/api/v1/rule_set_dir.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from constant import master, send_rule_set_dir_strip
-from core.config import CONTENT_FILE_PATH_BLACKLIST_STRIP
+from core.config import DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP, DEFAULT_RULE_SET_DIR
 from model.domain import Tag
 from service import RuleSetService, get_rule_set_service
 
@@ -9,8 +9,8 @@ rule_set_dir_router: APIRouter = APIRouter()
 
 
 @rule_set_dir_router.post(path=send_rule_set_dir_strip, tags=[master])
-def send_rule_set_dir(rule_set_dir: str,
-                      banned_strips: tuple[str, ...] = CONTENT_FILE_PATH_BLACKLIST_STRIP,
+def send_rule_set_dir(rule_set_dir: str = DEFAULT_RULE_SET_DIR,
+                      banned_strips: tuple[str, ...] = DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP,
                       rule_set_service: RuleSetService = Depends(get_rule_set_service)) -> list[Tag]:
     rule_set_service.import_rule_set(rule_set_dir, banned_strips)
     return rule_set_service.tags
