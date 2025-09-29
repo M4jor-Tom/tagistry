ef916c2bc73bc3dc58f5429a4ec559a10a6abf58
Tom <tom@fake.mail>
2025-09-29 22:58:29 +0200

    feat(content_file): detect unallowed tags

--- app/exception/content_import_exception.py
+++ app/exception/content_import_exception.py
@@ -1,18 +1,31 @@
 from exception import DomainException
 
+
 class ContentImportException(DomainException):
     path: str
+
+    def __init__(self, path: str):
+        self.path = path
+
 
 class FileNameWithoutSpaces(ContentImportException):
     reason = "tags part must have a space with the content title "
     "('<tags> <file_name>' instead of '<tags><file_name>')"
 
-    def __init__(self, path: str):
-        super().__init__(path)
+
+class TagNotAllowedException(ContentImportException):
+    reason = "tag not allowed found"
+    unknown_tag: str
+
+    def __init__(self, unknown_tag: str):
+        self.unknown_tag = unknown_tag
+
+    def get_details(self) -> str:
+        return f"the following tag is unallowed: {self.unknown_tag}"
 
 
 class UntaggedContentFile(ContentImportException):
     reason = "content file is untagged"
 
-    def __init__(self, path: str):
-        super().__init__(path)
+    def get_details(self) -> str:
+        return f"content file is untagged: {self.path}"
