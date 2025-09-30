a291f598c5c64270a3cd694a7ea20bde37b1ac10
Tom <tom@fake.mail>
2025-09-30 17:54:15 +0200

    feat(stage_up.sh): defined DEFAULT_CONTENT_FILE_DIR & DEFAULT_RULE_SET_DIR if missing

--- stage_up.sh
+++ stage_up.sh
@@ -3,4 +3,10 @@
 if [[ -f env.sh ]]; then
   source env.sh
 fi
+if [[ $DEFAULT_CONTENT_FILE_DIR == "" ]]; then
+  export DEFAULT_CONTENT_FILE_DIR="/var/tagistry/content_files"
+fi
+if [[ $DEFAULT_RULE_SET_DIR == "" ]]; then
+  export DEFAULT_RULE_SET_DIR="/var/tagistry/rule_set"
+fi
 docker compose up --build
