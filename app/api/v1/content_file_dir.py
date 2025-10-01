3b21d651116117698d82d046ef013998c66da992
Tom <tom@fake.mail>
2025-10-01 08:19:28 +0200

    refactor(content_file service): use dict instead of content_validation_summary with exceptinos keys/values

--- app/api/v1/content_file_dir.py
+++ app/api/v1/content_file_dir.py
@@ -3,7 +3,6 @@ from fastapi.params import Depends
 
 from core.config import DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP, DEFAULT_CONTENT_FILE_DIR
 from constant import master, send_content_file_parent_dir_strip
-from model.domain import ContentValidationSummary
 from service import ContentFileService, get_content_file_service
 
 content_file_dir_router = APIRouter()
@@ -14,7 +13,7 @@ def send_content_file_parent_dir(
         content_file_absolute_parent_dir: str = DEFAULT_CONTENT_FILE_DIR,
         banned_strips: tuple[str, ...] = DEFAULT_CONTENT_FILE_PATH_BLACKLIST_STRIP,
         compute_hash: bool = False,
-        content_file_service: ContentFileService = Depends(get_content_file_service)) -> ContentValidationSummary:
+        content_file_service: ContentFileService = Depends(get_content_file_service)) -> dict[str, list[str]]:
     return content_file_service.input_content_files(
         content_file_absolute_parent_dir,
         banned_strips,
--- app/exception/content_import_exception.py
+++ app/exception/content_import_exception.py
@@ -2,6 +2,8 @@ from exception import DomainException
 
 
 class ContentImportException(DomainException):
+    key: str
+    value: str
     path: str
 
     def __init__(self, path: str):
@@ -12,22 +14,34 @@ class ContentImportException(DomainException):
 
 
 class FileNameWithoutSpacesException(ContentImportException):
+    key = "file_names_without_spaces"
     reason = "tags part must have a space with the content title ('<tags> <file_name>' instead of '<tags><file_name>')"
+
+    def __init__(self, path: str):
+        super().__init__(path)
+        self.value = path
 
 
 class TagNotAllowedException(ContentImportException):
+    key = "tags_not_allowed"
     reason = "tag not allowed found"
     unknown_tag: str
 
     def __init__(self, unknown_tag: str):
         self.unknown_tag = unknown_tag
+        self.value = unknown_tag
 
     def get_details(self) -> str:
         return f"the following tag is unallowed: {self.unknown_tag}"
 
 
 class UntaggedContentFileException(ContentImportException):
+    key = "untagged_content_file_names"
     reason = "content file is untagged"
+
+    def __init__(self, path: str):
+        super().__init__(path)
+        self.value = path
 
     def get_details(self) -> str:
         return f"content file is untagged: {self.path}"
