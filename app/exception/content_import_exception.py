2eba7d925cca309b47676b5a6ae3d509c106de6f
Tom <tom@fake.mail>
2025-10-01 22:20:40 +0200

    refatctor(exceptions): key/value object mecanic inherits from domain exception type

--- app/exception/content_import_exception.py
+++ app/exception/content_import_exception.py
@@ -2,46 +2,19 @@ from exception import DomainException
 
 
 class ContentImportException(DomainException):
-    key: str
-    value: str
-    path: str
-
-    def __init__(self, path: str):
-        self.path = path
-
-    def get_details(self) -> str:
-        return f"{self.reason}: {self.path}"
+    pass
 
 
 class FileNameWithoutSpacesException(ContentImportException):
     key = "file_names_without_spaces"
     reason = "tags part must have a space with the content title ('<tags> <file_name>' instead of '<tags><file_name>')"
 
-    def __init__(self, path: str):
-        super().__init__(path)
-        self.value = path
-
 
 class TagNotAllowedException(ContentImportException):
     key = "tags_not_allowed"
-    reason = "tag not allowed found"
-    unknown_tag: str
-
-    def __init__(self, unknown_tag: str):
-        self.unknown_tag = unknown_tag
-        self.value = unknown_tag
-
-    def get_details(self) -> str:
-        return f"the following tag is unallowed: {self.unknown_tag}"
+    reason = "the following tag is unallowed"
 
 
 class UntaggedContentFileException(ContentImportException):
     key = "untagged_content_file_names"
     reason = "content file is untagged"
-
-    def __init__(self, path: str):
-        super().__init__(path)
-        self.value = path
-
-    def get_details(self) -> str:
-        return f"content file is untagged: {self.path}"
