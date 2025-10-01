ea8e5f0303ada7347de4ee43ed3706ce97fc928c
Tom <tom@fake.mail>
2025-10-02 00:10:13 +0200

    feat(rule_set service): detect inheritance tag elements which are absent from category dirs

--- app/exception/__init__.py
+++ app/exception/__init__.py
@@ -2,4 +2,4 @@ from .domain_exception import DomainException
 from .content_import_exception import ContentImportException, FileNameWithoutSpacesException, TagNotAllowedException, \
     UntaggedContentFileException
 from .rule_set_import_exception import RuleSetImportException, RuleSetElementPathDoesNotExist, \
-    InconsistentRuleSetException
+    InconsistentRuleSetCategoryInheritanceException, InheritanceTagsAbsentFromCategoriesTagsException
--- app/exception/rule_set_import_exception.py
+++ app/exception/rule_set_import_exception.py
@@ -9,20 +9,24 @@ class RuleSetElementPathDoesNotExist(RuleSetImportException):
     reason = "rule set element path does not exist"
 
 
-class InconsistentRuleSetException(RuleSetImportException):
-    reason = "rule set inconsistent"
+class InconsistentRuleSetCategoryInheritanceException(RuleSetImportException):
+    reason = "rule set's categories inheritance inconsistent"
     inconsistent_tag_with_mandatory_category: list[tuple[str, list[str]]]
 
     def __init__(self, inconsistent_tag_with_mandatory_category: list[tuple[str, list[str]]]):
         details_value: list[str] = [
-            InconsistentRuleSetException.get_inconsistent_tag_name_detail(inconsistency)
+            InconsistentRuleSetCategoryInheritanceException.get_inconsistent_tag_name_detail(inconsistency)
             for inconsistency
             in inconsistent_tag_with_mandatory_category
         ]
-        self.details = f"{self.reason}: {details_value}"
+        self.value = f"{details_value}"
 
     @staticmethod
     def get_inconsistent_tag_name_detail(inconsistent_tag_with_mandatory_category: tuple[str, list[str]]) -> str:
         inconsistent_tag_value: str = inconsistent_tag_with_mandatory_category[0]
         mandatory_categories: list[str] = inconsistent_tag_with_mandatory_category[1]
         return f"{inconsistent_tag_value} should have the following: {mandatory_categories} categories"
+
+
+class InheritanceTagsAbsentFromCategoriesTagsException(RuleSetImportException):
+    reason = "in inheritance dir, found tags absent from category dir"
