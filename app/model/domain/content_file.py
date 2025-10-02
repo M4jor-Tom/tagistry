55b115ef5c02852b5909561bb891c55268896617
Tom <tom@fake.mail>
2025-10-02 23:46:21 +0200

    refactor(content_file model): path becomes a computed property

--- app/model/domain/content_file.py
+++ app/model/domain/content_file.py
@@ -4,7 +4,18 @@ from pydantic import BaseModel
 
 
 class ContentFile(BaseModel):
-    path: str
+    dir_name: str
+    base_name_without_extension: str
+    extension: str
     content_hash: str | None
     tag_values: list[str]
     missing_tag_values: list[str]
+
+    @property
+    def path(self) -> str:
+        base_name: str = f"{self.base_name_without_extension}.{self.extension}"\
+            if self.extension != ""\
+            else self.base_name_without_extension
+        if self.dir_name != "" and base_name != "":
+            return f"{self.dir_name}/{base_name}"
+        return base_name
